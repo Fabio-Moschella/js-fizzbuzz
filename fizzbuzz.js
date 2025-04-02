1be79b7a88d6ec84d7454e426ff1ad6d051f861e
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-02 15:10:13 +0200

    first commit

---
+++ fizzbuzz.js
@@ -0,0 +1,7 @@
+// DATI
+const multipliDi3 = "Fizz";
+const multipliDi5 = "Buzz";
+const multipli3E5 = "FizzBuzz";
+
+// se l indice e uguale a 1 e i multiplidi3 sono maggiori di 3 allora i aumenta di 2..
+for (let i = 1; multipliDi3 > 3; i = i + 3);
