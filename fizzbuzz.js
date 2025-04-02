26823ba766f4e99998847689ea557e731436e891
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-04-02 16:30:50 +0200

    risolto-bug-fizz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -6,20 +6,19 @@ const multipli3E5 = "FizzBuzz";
 // se l indice e` uguale a 1 e l indice e` minore di 0, l`indice aumenta di 1
 for (let i = 1; i <= 100; i++) {
   //ed in console stampa i numeri da 1 a 100
-  //console.log(i);
-
+  // se il numero e` multiplo sia di 3 che di 5
+  if (i % 3 === 0 && i % 5 === 0) {
+    // stampo FizzBUZZ
+    console.log(multipli3E5);
+  }
   // Se il numero è multiplo di 3
-  if (i % 3 === 0) {
+  else if (i % 3 === 0) {
     //   Stampo Fizz
     console.log(multipliDi3);
     // Se invece il numero è multiplo  di 5
   } else if (i % 5 === 0) {
     //   Stampo Buzz
     console.log(multipliDi5);
-    // Se invece il numero è multiplo di 3 e 5 (quindi di 15)
-  } else if (i % 3 === 0 && i % 5 === 0) {
-    // stampo FizzBUZZ
-    console.log(multipli3E5);
     // Altrimenti
   } else {
     //   Stampo i
